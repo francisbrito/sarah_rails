c9b93c4f26bc0210f22240bfaae90aca36f8de2b
Francis Brito <fr.br94@gmail.com>
2014-07-21 23:43:10 -0400

    Scaffold Omniauth callbacks.

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,17 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+        # TODO: Save Google token.
+    end
+
+    def facebook
+        # TODO: Save Facebook token.
+    end
+
+    def twitter
+        # TODO: Save Twitter token.
+    end
+
+    def github
+        # TODO: Save Github token.
+    end
+end
