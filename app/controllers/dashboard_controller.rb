1e9c725b2b90d632cc0c2541aa063b7ea8436eca
Francis Brito <fr.br94@gmail.com>
2014-07-22 04:25:46 -0400

    Add list of messages to dashboard view.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,8 @@ class DashboardController < ApplicationController
 
   def index
       @integrated_with_google = session['devise.google'].nil? 
+
+      # Retrieve all messages for current user.
+      @messages = Message.where user_id: current_user.id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722022836) do
+ActiveRecord::Schema.define(version: 20140722040902) do
 
   create_table "messages", force: true do |t|
     t.string   "content"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20140722022836) do
     t.string   "provider"
     t.string   "uid"
     t.string   "phone_number"
+    t.string   "oauth_facebook"
+    t.string   "oauth_google"
+    t.string   "oauth_twitter"
+    t.string   "oauth_github"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
