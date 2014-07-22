45fe804b59bef607de14df4bf7c68eb5858994ba
Francis Brito <fr.br94@gmail.com>
2014-07-22 03:58:59 -0400

    Integration with Google Auth works now.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
   def index
+      @integrated_with_google = session['devise.google'].nil? 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter, :github]
 
   has_many :messages
+
+  def update_from_google_oauth_hash(oauth_hash)
+      require 'pry'; binding.pry
+      self.email ||= oauth_hash.info.email
+      self.oauth_google = oauth_hash.credentials.token
+      self.save
+      self
+  end
 end
