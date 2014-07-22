e81702036128aa147ff6117bcb70c168e789c886
aqt01 <lowell.abbott@gmail.com>
2014-07-22 03:25:43 -0400

    Added class to reply to whatsapp contact

---
+++ lib/baseworker.rb
@@ -0,0 +1,27 @@
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+# Response to whatsapp contact
+class Response
+	def send_response(phone_number, content)
+
+	uri = URI.parse("http://localhost:5000/message")
+
+	header = {'Content-Type'=> 'text/json'}
+	user =  {                   
+                    'msg'=> content,
+                    'number'=> phone_number
+             }
+				http = Net::HTTP.new(uri.host, uri.port)
+				request = Net::HTTP::Post.new(uri.request_uri, header)
+				request.body = user.to_json
+				response = http.request(request)
+	end
+
+	end
+
+# Send the request
+resp = Response.new()
+resp.send_response('18298647935','Sup, this works')	
